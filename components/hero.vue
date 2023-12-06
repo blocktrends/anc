3ea47e1dadedfe72cc91444ddd25d149d550f0b9
Marco Lima <marco.lima@qr.capital>
2023-12-06 17:25:00 -0300

    feat: landing entrega 1

--- components/hero.vue
+++ components/hero.vue
@@ -1,61 +1,42 @@
 <template>
-    <div class="py-10"  style="background: linear-gradient(222.77deg, #0296bb 1.96%, #39d8ff 98.36%);">
-        <div
-  class="justify-center items-center self-stretch flex w-full flex-col px-16 py-12 max-md:max-w-full max-md:px-5"
->
-  <div
-    class="items-stretch flex w-full max-w-[1534px] justify-between gap-5 max-md:max-w-full max-md:flex-wrap max-md:justify-center"
-  >
-    <div class="items-center flex justify-between gap-3">
-      <img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/06845989419dac288bc0e20c7da9ea128d19d1581a42f20ca0a3e724e38c2a8e?apiKey=0416d2cb0fb64e0d89568a994dd4a754&"
-        class="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full my-auto"
-      />
+  <div style="background: linear-gradient(222.77deg, #0296bb 1.96%, #39d8ff 98.36%);">
+    <div class="xl:container mx-auto justify-center items-center self-stretch flex w-full flex-col px-5 2xl:px-0  max-md:max-w-full ">
       <div
-        class="text-slate-950 text-2xl font-semibold leading-7 self-stretch grow whitespace-nowrap"
-      >
-        Curso intensivo
+        class="items-stretch py-7 md:py-12 flex w-full max-w-[1534px] justify-between gap-5 max-w-full flex-wrap justify-center">
+        <div class="items-center flex justify-between gap-3">
+          <img loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/06845989419dac288bc0e20c7da9ea128d19d1581a42f20ca0a3e724e38c2a8e?apiKey=0416d2cb0fb64e0d89568a994dd4a754&"
+            class="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full my-auto" />
+          <div class="text-slate-950 text-xs lg:text-lg xl:text-xl 2xl:text-2xl font-semibold leading-7 self-stretch grow whitespace-nowrap">
+            Curso intensivo
+          </div>
+        </div>
+        <div class="items-center flex justify-between gap-3">
+          <img loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/65bf35b908b4acf1b96e8023c532782151f1042fd5ec138efd2511913cecac6e?apiKey=0416d2cb0fb64e0d89568a994dd4a754&"
+            class="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full my-auto" />
+          <div class="text-slate-950 text-xs lg:text-lg xl:text-xl 2xl:text-2xl font-semibold leading-7 self-stretch grow whitespace-nowrap">
+            Certificação ANCORD
+          </div>
+        </div>
+        <div class="items-center flex justify-between gap-3">
+          <img loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/23b31e36618efcfce08e4c1a2f27fb07a5322a70c7f7852fbfc1b31b52a02772?apiKey=0416d2cb0fb64e0d89568a994dd4a754&"
+            class="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full my-auto" />
+          <div class="text-slate-950 text-xs lg:text-lg xl:text-xl 2xl:text-2xl font-semibold leading-7 self-stretch grow whitespace-nowrap">
+            Atendimento especializado
+          </div>
+        </div>
+        <div class="items-center flex justify-between gap-3">
+          <img loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/7df283a7eb1fca3cc56aa5313bc87c2cb104c67508f186bf60ca4a837768e6ba?apiKey=0416d2cb0fb64e0d89568a994dd4a754&"
+            class="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full my-auto" />
+          <div class="text-slate-950 text-xs lg:text-lg xl:text-xl 2xl:text-2xl font-semibold leading-7 self-stretch grow whitespace-nowrap">
+            Comunidade exclusiva
+          </div>
+        </div>
       </div>
     </div>
-    <div class="items-center flex justify-between gap-3">
-      <img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/65bf35b908b4acf1b96e8023c532782151f1042fd5ec138efd2511913cecac6e?apiKey=0416d2cb0fb64e0d89568a994dd4a754&"
-        class="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full my-auto"
-      />
-      <div
-        class="text-slate-950 text-2xl font-semibold leading-7 self-stretch grow whitespace-nowrap"
-      >
-        Certificação ANCORD
-      </div>
-    </div>
-    <div class="items-center flex justify-between gap-3">
-      <img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/23b31e36618efcfce08e4c1a2f27fb07a5322a70c7f7852fbfc1b31b52a02772?apiKey=0416d2cb0fb64e0d89568a994dd4a754&"
-        class="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full my-auto"
-      />
-      <div
-        class="text-slate-950 text-2xl font-semibold leading-7 self-stretch grow whitespace-nowrap"
-      >
-        Atendimento especialiazado
-      </div>
-    </div>
-    <div class="items-center flex justify-between gap-3">
-      <img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/7df283a7eb1fca3cc56aa5313bc87c2cb104c67508f186bf60ca4a837768e6ba?apiKey=0416d2cb0fb64e0d89568a994dd4a754&"
-        class="aspect-square object-contain object-center w-6 overflow-hidden shrink-0 max-w-full my-auto"
-      />
-      <div
-        class="text-slate-950 text-2xl font-semibold leading-7 self-stretch grow whitespace-nowrap"
-      >
-        Comunidade exclusiva
-      </div>
-    </div>
-  </div>
-</div>
 
-            </div>
+  </div>
 </template>
