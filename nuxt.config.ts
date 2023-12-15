96887db295525aa7cc99cc3e072abc44f002dfe8
Marco Lima <marco.lima@qr.capital>
2023-12-15 16:45:55 -0300

    feat: fb pixel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,7 +35,34 @@ export default defineNuxtConfig({
         {  hid: 'title', name: 'title', content: 'Programa Certificação Criptoativos ANCORD'},
         {  hid: 'og:title', name: 'twitter:title', content: 'Programa Certificação Criptoativos ANCORD'},
         {  hid: 'description', name: 'description', content: 'Domine a blockchain e lidere a revolução cripto com o Programa CCA, desenvolvido pelo BlockTrends e certificado pela ANCORD.'}
-      ]
+      ],
+      script: [
+        {
+          hid: 'clt',
+          innerHTML: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "k6gbmyo5mo");`,
+        },
+        {
+          hid: 'fb',
+          innerHTML: `
+          !function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '322393097349578');
+fbq('track', 'PageView');
+`,
+        },
+        
+      ],
     },
   },
 })
