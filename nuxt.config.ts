65f2414a57beeb568d00901acdb8dd14fdb7a127
Marco Lima <marco.lima@qr.capital>
2023-12-15 16:07:07 -0300

    feat: fb pixel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,7 +35,27 @@ export default defineNuxtConfig({
         {  hid: 'title', name: 'title', content: 'Programa Certificação Criptoativos ANCORD'},
         {  hid: 'og:title', name: 'twitter:title', content: 'Programa Certificação Criptoativos ANCORD'},
         {  hid: 'description', name: 'description', content: 'Domine a blockchain e lidere a revolução cripto com o Programa CCA, desenvolvido pelo BlockTrends e certificado pela ANCORD.'}
-      ]
+      ],
+      script: [
+        {
+          hid: 'metaFb',
+          innerHTML: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1079915489855328');
+          fbq('track', 'PageView');
+          </script>
+          <noscript><img height="1" width="1" style="display:none"
+          src="https://www.facebook.com/tr?id=1079915489855328&ev=PageView&noscript=1"
+          /></noscript>`,
+        }
+      ],
     },
   },
 })
