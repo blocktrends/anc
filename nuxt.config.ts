55fc8952fb65312e6797baf8dae3658bf0e8df30
root <root@QRRJ0012.localdomain>
2025-01-27 10:51:54 -0300

    feat: GA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -64,6 +64,19 @@ export default defineNuxtConfig({
         })(window, document, "clarity", "script", "k6gbmyo5mo");`,
         },
         {
+          src: "https://www.googletagmanager.com/gtag/js?id=G-ER5C2HT555",
+          async: true,
+        },
+        {
+          hid: 'gtag-inline',
+          innerHTML: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag() { dataLayer.push(arguments); }
+            gtag('js', new Date());
+            gtag('config', 'G-ER5C2HT555');
+          `,
+        },
+        {
           hid: 'fb',
           innerHTML: `
           !function(f,b,e,v,n,t,s)
@@ -77,7 +90,22 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
 fbq('init', '322393097349578');
 fbq('track', 'PageView');
 `
-        },       
+        },    
+        {
+          hid: 'ga',
+          innerHTML: `
+          !function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '322393097349578');
+fbq('track', 'PageView');
+`
+        },     
         {
           hid: 'fb',
           innerHTML: `
